35dd53e7452344f698afdea597948ca8a76e9521
ctslone <ctslone27@gmail.com>
2020-01-26 21:11:48 -0500

    initial commit with working localhost dev server

---
+++ server.js
@@ -0,0 +1,35 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongojs = require("mongojs");
+var mongoose = require("mongoose");
+
+// Initialize Express
+var app = express();
+
+// models
+// var db = require("./models")
+
+// server
+var PORT = process.env.PORT || 3000;
+
+// setting up static folder for public html and css AND parsing req body as JSON
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// setting up handlebars main view
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// connect to mongoDB using mongoose
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/buildsheetdb"
+mongoose.connect(MONGODB_URI);
+
+app.get("/", function(req, res) {
+    res.send("working")
+})
+
+app.listen(PORT, function () {
+    console.log("App running on port 3000!");
+});
